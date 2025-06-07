0d7add376c3c3329a34fac91c7b27eaf4901412f
Lucas Safeno <lugnorio@gmail.com>
2025-06-07 11:06:05 -0300

    Criando banco de dados e conexão - PHP Profissional Orientado a Objetos #12

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace app\controllers;
 
+use app\database\models\User;
 use app\controllers\Controller;
 
 class HomeController extends Controller
 {
   public function index()
   {
+    $user = new User;
+    $user->fetch();
+
     $this->view('home', [
       'title' => 'Home',
       'name' => 'safeno'
